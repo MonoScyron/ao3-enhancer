01664a4f0955589012667ea454032a8346e00401
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2022-12-23 23:24:19 -0500

    Menu finished for now

--- options/options.js
+++ options/options.js
@@ -1,18 +1,45 @@
 // * Storage keys
-const blockTagKey = "exclude-tag";
+// Kudos to hit ratio
 const kudosHitRatioKey = "kudos-hit-ratio";
+// Language
+const languageEnableKey = "language-enable";
 const languageKey = "language";
 
+// * Select input from document
+// Kudos to hit ratio
 let kudosHitRatioBtn = document.querySelector(`input[name='${kudosHitRatioKey}']`);
-
+// Language
+let languageBtn = document.querySelector(`input[name='${languageEnableKey}']`);
 let languageSelect = document.querySelector(`select[name='${languageKey}']`);
-let languageBtn = document.querySelector(``);
 
-kudosHitRatioBtn.checked = browser.storage.sync.get(kudosHitRatioKey);
+// * Sync inputs to values saved in storage
+// Kudos to hit ratio
+browser.storage.local.get(kudosHitRatioKey).then((value) => {
+    kudosHitRatioBtn.checked = value == "true";
+});
+// Language
+browser.storage.local.get(languageEnableKey).then((value) => {
+    languageBtn.checked = value == "true";
+});
+browser.storage.local.get(languageKey).then((value) => {
+    languageSelect.value = value;
+});
 
-// * Every button should save settings to local storage when clicked
+// * Save settings to local storage when value is changed
+// Kudos to hit ratio
 kudosHitRatioBtn.addEventListener("click", () =>
     browser.storage.local.set(
         { kudosHitRatioKey: kudosHitRatioBtn.checked }
+    )
+);
+// Language
+languageBtn.addEventListener("click", () =>
+    browser.storage.local.set(
+        { languageEnableKey: languageBtn.checked }
+    )
+);
+languageSelect.addEventListener("change", () =>
+    browser.storage.local.set(
+        { languageKey: languageSelect.value }
     )
 );
